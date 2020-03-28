9c0ecefc9496849d4512344f65db1ba93f60fc14
eyakub <eyakubsorkar@gmail.com>
2020-03-29 00:56:53 +0600

    Updates

---
+++ precache-manifest.08151f6da8c4b5115998ea5b03ce5e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a469072cf226d58a43b82a9465c028ae",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "4d6887a2cc079761b2c8",
+    "url": "/movie-app-react/static/css/main.d6fe937b.chunk.css"
+  },
+  {
+    "revision": "da63b1f8dba0da766979",
+    "url": "/movie-app-react/static/js/2.7ac05915.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movie-app-react/static/js/2.7ac05915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6887a2cc079761b2c8",
+    "url": "/movie-app-react/static/js/main.0fa40481.chunk.js"
+  },
+  {
+    "revision": "81002160cf89b8f6be01",
+    "url": "/movie-app-react/static/js/runtime-main.784836f8.js"
+  }
+]);
